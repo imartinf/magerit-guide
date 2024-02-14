f773a023c0f7e0e0dd977a8aaf87735ba159baa2
Sergio <sergio.estebanro@alumnos.upm.es>
2024-02-14 12:26:46 +0100

    Fixed: python file minor errors

--- probe_magerit.sh
+++ probe_magerit.sh
@@ -2,19 +2,21 @@
 ##----------------------- Start job description -----------------------
 #SBATCH --job-name=prueba
 #SBATCH --nodes=1
+#SBATCH --mem-per-cpu=12G
 #SBATCH --partition=standard-gpu
-#SBATCH --output=/home/<code>/<user>/logs/%j.out
+#SBATCH --gres=gpu:1
+#SBATCH --output=/home/<code>/<user>/magerit-guide/%j.out
 #SBATCH --mail-user=<your_mail>
 ##------------------------ End job description ------------------------
 
 # Load modules
-module purge && module load Python
+module purge && module load Python/3.10.8-GCCcore-12.2.0
 
 # Activate virtual environment
 source venv/bin/activate
 
 # Install dependencies
-pip install -r requirements.txt # Only use the first launch
+# pip install -r requirements.txt # Only use the first launch
 
 # Run script
-srun python scripts/probe_magerit.py
+srun python probe_magerit.py
